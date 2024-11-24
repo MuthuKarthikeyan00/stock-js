785ee3af212bf4d716b5b33cd80cc93bf43961e3
Muthu Karthikeyan <muthukarthikeyan00@gmail.com>
2024-11-24 23:58:42 +0530

    chnages

--- src/helpers/Sanitizer.js
+++ src/helpers/Sanitizer.js
@@ -3,11 +3,24 @@ const sanitizer = require('sanitizer');
 class Sanitizer {
 
     static escape(text) {
-        return sanitizer.escape(String(text));
+        return sanitizer.escape(text);
     }
 
     static sanitize(text) {
-        return sanitizer.sanitize(String(text));
+        return sanitizer.sanitize(text);
+    }
+
+    static sanitizeString(text) {
+        return Sanitizer.escape(String(text));
+    }
+
+    static async sanitizeHtml(params, exclude = []) {
+        Object.keys(params).forEach((key) => {
+            if (params.hasOwnProperty(key) && typeof params[key] === 'string' && !exclude.includes(key)) {
+                params[key] = Sanitizer.sanitizeString(params[key]);
+            }
+        });
+        return params;
     }
 }
 
